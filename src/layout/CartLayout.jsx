561dd93b73ede5e94ff291148d471024f2e69055
papa2415 <167390811+papa2415@users.noreply.github.com>
2026-02-04 11:42:19 +0800

    fix context

--- src/layout/CartLayout.jsx
+++ src/layout/CartLayout.jsx
@@ -8,7 +8,7 @@ function CartLayout() {
       <header>
         <div className="container">
           <div className="d-flex justify-content-between py-4">
-            <NavLink to="/" className="h3 d-flex align-items-center" context={{ isAuth }}>
+            <NavLink to="/" className="h3 d-flex align-items-center">
               <img src={mark} alt="logo" className="me-2" />
               <span className="text-underline">觀葉森活</span>
             </NavLink>
